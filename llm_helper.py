009aa9c6611ba200cd196225455329cca8ea140c
Anuja khatri <70836354+Anujakhatri@users.noreply.github.com>
2025-08-06 21:41:26 +0545

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -9,4 +9,3 @@ llm= ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name ="llama-3.3-70b
 if __name__=="__main__":
     response = llm.invoke("What are the two main ingredients in samosa")
     print(response.content)
-
